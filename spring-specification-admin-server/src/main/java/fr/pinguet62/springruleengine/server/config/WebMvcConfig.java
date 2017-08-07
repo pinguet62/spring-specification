3b0d6e6eee6cd67db79df8d637de034aec13b15a
Pinguet62 <pinguet62@gmail.com>
2017-08-07 22:48:56 +0200

    Packaging of Angular UI

---
+++ spring-specification-admin-server/src/main/java/fr/pinguet62/springruleengine/server/config/WebMvcConfig.java
@@ -0,0 +1,35 @@
+package fr.pinguet62.springruleengine.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Configure <b>Spring Boot</b> embedded Angular website.
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * Handle resources.
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/spring-specification-admin-client/")
+                .resourceChain(true);
+    }
+
+    /**
+     * Define root URL.
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+
+        registry.addViewController("/").setViewName("index.html");
+    }
+}
